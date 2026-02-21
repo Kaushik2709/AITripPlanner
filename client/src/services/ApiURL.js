5d3475c69957b38b13491736c605e3a9e74516d2
Kaushik2709 <kaushik2005m@gmail.com>
2026-02-21 10:58:51 +0530

    frontend url added

--- client/src/services/ApiURL.js
+++ client/src/services/ApiURL.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL =
     import.meta.env.VITE_API_URL ||
-    (import.meta.env.PROD ? "/api" : "http://localhost:3000/api");
+    (import.meta.env.PROD ? "/api" : "https://aitripplanner-backend-8ln1.onrender.com/api");
 
 const api = axios.create({
     baseURL: BASE_URL,
